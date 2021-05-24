6ff577cc02ffa63805823c9e8512722388b8abe2
Justyna <zadora990605@gmail.com>
2021-05-24 17:42:18 +0200

    Removing enemies after shot

--- index.js
+++ index.js
@@ -47,6 +47,29 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
 
 let x = canvas.width / 2;
 const y = canvas.height * 0.9;
@@ -54,6 +77,54 @@ const y = canvas.height * 0.9;
 const player = new Player(x, y, 30, 'blue');
 const projectiles = [];
 const enemies = [];
+
+// function spawnEnemies() {
+//     setInterval(() => {
+//         const radius = Math.random() * (30 - 15) + 15;
+//
+//         let x;
+//         let y;
+//
+//         if (Math.random() < 0.5) {
+//             x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+//             y = Math.random() * canvas.height;
+//         } else {
+//             x = Math.random() * canvas.width;
+//             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+//         }
+//
+//         const color = 'green';
+//         const angle = Math.atan2(
+//             canvas.height / 2 - y,
+//             canvas.width / 2 - x);
+//         const velocity = {
+//             x: Math.cos(angle),
+//             y: Math.sin(angle)
+//         }
+//
+//         enemies.push(new Enemy(x, y, radius, color, velocity));
+//         console.log(enemies);
+//     }, 1000);
+// }
+
+function spawnEnemies() {
+    setInterval(() => {
+
+        const x = Math.floor(Math.random() * ((canvas.width - 30) - 30) ) + 30;
+        const y = 0;
+        const radius = Math.random() * (30 - 15) + 15;
+        const color = 'green';
+        const velocity = {
+            x: 0,
+            y: 1
+        }
+
+        enemies.push(new Enemy(x, y, radius, color, velocity));
+        console.log(enemies);
+    }, 3000);
+}
+
+
 
 
 function animate() {
@@ -65,6 +136,22 @@ function animate() {
     })
     enemies.forEach((enemy, index) => {
         enemy.update()
+
+        const dist = canvas.height - enemy.y;
+        if (dist < 1) {
+            console.log('end game');
+        }
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+
+            if (dist - enemy.radius - projectile.radius < 1) {
+                setTimeout(() => {
+                    enemies.splice(index, 1);
+                    projectiles.splice(projectileIndex, 1);
+                }, 0)
+            }
+        })
     })
 }
 
@@ -87,24 +174,27 @@ function move(e) {
     }
 }
 
-addEventListener('click', shoot, false);
+addEventListener('keyup', shoot, false);
 
 function shoot(e) {
 
-    const angle = Math.atan2(-90,
-        0);
+    if (e.keyCode === 32) {
+        const angle = Math.atan2(-90,
+            0);
 
-    const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+        projectiles.push(
+            new Projectile(player.x, player.y, 5, 'red', velocity)
+        );
     }
-
-    projectiles.push(
-        new Projectile(player.x, player.y, 5, 'red', velocity)
-    );
 
 }
 
 
 animate();
+spawnEnemies();
 
